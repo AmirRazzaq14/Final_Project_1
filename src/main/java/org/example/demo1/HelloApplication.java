a0e84c6098c42e7c28ee42420ae0cb62ae7fcad5
amirrazzaq <amirrazzaq12@gmail.com>
2025-09-09 18:46:17 -0400

    Welcome to javafx

--- src/main/java/org/example/demo1/HelloApplication.java
+++ src/main/java/org/example/demo1/HelloApplication.java
@@ -6,15 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello, Welcome to JavaFX!");
         stage.setScene(scene);
         stage.show();
-        //github
+        //github 123
     }
 }
