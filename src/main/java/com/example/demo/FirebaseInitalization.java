4160d9bcea763e86e2fc221e88cc18d05a16f251
Maxwellkat <maxwellkatz@gmail.com>
2025-11-24 19:32:29 -0500

    Working on firebase dependencies

---
+++ src/main/java/com/example/demo/FirebaseInitalization.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FirebaseInitalization {
+    private static boolean initialized = false;
+
+    public Firestore firebase() {
+        if (!initialized) {
+            initializeFirebase();
+        }
+        return FirestoreClient.getFirestore();
+    }
+    private static void initializeFirebase() {
+try{
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/com/example/demo/key.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://console.firebase.google.com/u/0/project/shapeshift-184c8/database/shapeshift-184c8-default-rtdb/data/~2F")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires firebase.admin;
+    requires com.google.auth.oauth2;
+    requires google.cloud.firestore;
 
 
     opens com.example.demo to javafx.fxml;
